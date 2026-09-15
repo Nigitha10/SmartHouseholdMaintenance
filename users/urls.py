2cce59f49ebc08fd671fb7c728b373086f7097e4
Nigitha K <nigitharaja@gmail.com>
2026-09-15 22:21:19 +0530

    Add home page route

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path("", views.user_login, name="home"),
     path("register/", views.register, name="register"),
     path("login/", views.user_login, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
